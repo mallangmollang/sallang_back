19915b0a2904d4aaeb2dd65d401928c7672ec095
Yun <mosy2266@naver.com>
2025-08-13 16:58:15 +0900

    first commit

---
+++ src/main/java/mallang/sallang/SallangApplication.java
@@ -0,0 +1,13 @@
+package mallang.sallang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SallangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SallangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mallang/sallang/SallangApplicationTests.java
@@ -0,0 +1,13 @@
+package mallang.sallang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SallangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
